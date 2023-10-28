6b283d58e7e3b814e7d723bfebb38f1859c83fc1
nohy6630 <nohy6630@gmail.com>
2023-10-29 08:20:34 +0900

    fix: 에러 메시지 출력 수정

--- utils.c
+++ utils.c
@@ -6,16 +6,16 @@
 /*   By: yenoh <yenoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 20:30:50 by yenoh             #+#    #+#             */
-/*   Updated: 2023/10/27 23:20:30 by yenoh            ###   ########.fr       */
+/*   Updated: 2023/10/29 08:15:10 by yenoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	init_info(t_info *info, char *map)
+void init_info(t_info *info, char *map)
 {
-	int	img_len;
-	int	fd;
+	int img_len;
+	int fd;
 
 	fd = open(map, O_RDONLY);
 	if (fd == -1)
@@ -29,20 +29,20 @@ void	init_info(t_info *info, char *map)
 	info->img_len = 64;
 	info->mlx = mlx_init();
 	info->g_img = mlx_xpm_file_to_image(info->mlx,
-			"textures/g.xpm", &img_len, &img_len);
+										"textures/g.xpm", &img_len, &img_len);
 	info->w_img = mlx_xpm_file_to_image(info->mlx,
-			"textures/w.xpm", &img_len, &img_len);
+										"textures/w.xpm", &img_len, &img_len);
 	info->p_img = mlx_xpm_file_to_image(info->mlx,
-			"textures/p.xpm", &img_len, &img_len);
+										"textures/p.xpm", &img_len, &img_len);
 	info->c_img = mlx_xpm_file_to_image(info->mlx,
-			"textures/c.xpm", &img_len, &img_len);
+										"textures/c.xpm", &img_len, &img_len);
 	info->e_img = mlx_xpm_file_to_image(info->mlx,
-			"textures/e.xpm", &img_len, &img_len);
+										"textures/e.xpm", &img_len, &img_len);
 }
 
-void	free_info(t_info *info)
+void free_info(t_info *info)
 {
-	int	i;
+	int i;
 
 	i = -1;
 	while (++i < info->h)
@@ -50,20 +50,20 @@ void	free_info(t_info *info)
 	free(info->map);
 }
 
-void	perror_exit(char *opt)
+void perror_exit(char *opt)
 {
-	perror(opt);
+	write(2, opt, ft_strlen(opt));
 	exit(EXIT_FAILURE);
 }
 
-void	perror_free_exit(char *opt, t_info *info)
+void perror_free_exit(char *opt, t_info *info)
 {
-	perror(opt);
+	write(2, opt, ft_strlen(opt));
 	free_info(info);
 	exit(EXIT_FAILURE);
 }
 
-int	mlx_destroy_exit(t_info *info)
+int mlx_destroy_exit(t_info *info)
 {
 	mlx_destroy_window(info->mlx, info->win);
 	free_info(info);
